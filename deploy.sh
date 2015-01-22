156011ccecdcf213ee2d19d8d257bb45b2ac8736
sibymathew <siby.math@gmail.com>
2015-01-21 18:28:22 -0800

    Test1

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,8 @@
 
 docker push sibymath/circletest:v2
 
+cp /home/ubuntu/.aws/credentials /home/ubuntu/.awssecret
+
 EB_S3_BUCKET=siby-circletest-s3
 aws s3 cp Dockerrun.aws.json s3://$EB_S3_BUCKET/Dockerrun.aws.json
 
