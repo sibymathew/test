809809668eabf501d6ae1ed775acf727ff932f36
sibymathew <siby.math@gmail.com>
2015-01-21 19:13:21 -0800

    Test1

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ cp /home/ubuntu/.aws/credentials /home/ubuntu/.awssecret
 chmod 400 /home/ubuntu/.awssecret
 
 EB_S3_BUCKET=siby-circletest-s3
-#aws s3 cp Dockerrun.aws.json s3://$EB_S3_BUCKET/Dockerrun.aws.json
+aws s3 cp Dockerrun.aws.json s3://$EB_S3_BUCKET/Dockerrun.aws.json
 
 aws elasticbeanstalk create-application-version --application-name siby-circletest --version-label v1 --source-bundle S3Bucket=$EB_S3_BUCKET,S3Key=Dockerrun.aws.json
 
