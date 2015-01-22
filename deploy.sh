6b0ae8d8e30ed07f0ba1c553959a15c940c3b911
sibymathew <siby.math@gmail.com>
2015-01-21 17:31:52 -0800

    CircleTest

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+version=$1
+
+docker push siby/circletest:$version
+
+EB_S3_BUCKET=siby-circletest-s3
+aws s3 cp Dockerrun.aws.json s3://$EB_S3_BUCKET/Dockerrun.aws.json
+
+aws elasticbeanstalk create-application-version --application-name siby-circletest --version-label $version --source-bundle S3Bucket=$EB_S3_BUCKET,S3Key=Dockerrun.aws.json
+
+aws elasticbeanstalk update-environment --environment-name siby-circletest-env --version-label $version
