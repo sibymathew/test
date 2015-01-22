53f65f3d9fbd22c81e1103ea36908f075021b02d
sibymathew <siby.math@gmail.com>
2015-01-22 11:07:19 -0800

    Test1

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,6 @@ aws configure set region us-west-2
 EB_S3_BUCKET=siby-circletest-s3
 aws s3 cp Dockerrun.aws.json s3://$EB_S3_BUCKET/Dockerrun.aws.json
 
-aws elasticbeanstalk create-application-version --application-name siby-circletest --version-label v1 --source-bundle S3Bucket=$EB_S3_BUCKET,S3Key=Dockerrun.aws.json
+aws elasticbeanstalk create-application-version --application-name siby-circletest --version-label v2 --source-bundle S3Bucket=$EB_S3_BUCKET,S3Key=Dockerrun.aws.json
 
-aws elasticbeanstalk update-environment --environment-name siby-circletest-env --version-label v1
+aws elasticbeanstalk update-environment --environment-name sibycircletest-env --version-label v2
