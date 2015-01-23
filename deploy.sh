f64c56516c97484c1fd42b301f7af619296109d9
sibymathew <siby.math@gmail.com>
2015-01-22 17:09:35 -0800

    Test1

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,8 @@ EB_S3_BUCKET=siby-circletest-s3
 aws s3 cp Dockerrun.aws.json s3://$EB_S3_BUCKET/Dockerrun.aws.json
 aws s3 cp dockercfg s3://$EB_S3_BUCKET/docker/dockercfg
 
-aws elasticbeanstalk create-application --application-name $APP_NAME --environment-name $ENV_NAME
+aws elasticbeanstalk create-application --application-name $APP_NAME
+aws elasticbeanstalk create-environment --environment-name $ENV_NAME --application-name $APP_NAME
 
 aws elasticbeanstalk create-application-version --application-name $APP_NAME --version-label $VERSION --source-bundle S3Bucket=$EB_S3_BUCKET,S3Key=Dockerrun.aws.json
 
