92514c642433beb5f064cac9f460326a4bf16d08
sibymathew <siby.math@gmail.com>
2015-01-23 11:38:26 -0800

    Test1

--- deploy.sh
+++ deploy.sh
@@ -13,8 +13,7 @@ aws s3 cp Dockerrun.aws.json s3://$EB_S3_BUCKET/Dockerrun.aws.json
 aws s3 cp dockercfg s3://$EB_S3_BUCKET/docker/dockercfg
 
 aws elasticbeanstalk create-application --application-name $APP_NAME
-aws elasticbeanstalk create-environment --environment-name $ENV_NAME --application-name $APP_NAME
-
 aws elasticbeanstalk create-application-version --application-name $APP_NAME --version-label $VERSION --source-bundle S3Bucket=$EB_S3_BUCKET,S3Key=Dockerrun.aws.json
 
+aws elasticbeanstalk create-environment --environment-name $ENV_NAME --application-name $APP_NAME --solution-stack-name "64bit Amazon Linux 2014.09 v1.0.11 running Docker 1.3.3" --cname $ENV_NAME --option-settings file://options.txt
 aws elasticbeanstalk update-environment --environment-name $ENV_NAME --version-label $VERSION
