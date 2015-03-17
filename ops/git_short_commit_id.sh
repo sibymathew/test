7f5eb14e8df510ca7d7cde413f89c9648c9fea2c
sibymathew <siby.math@gmail.com>
2015-03-17 11:47:54 -0700

    Siby Deploy

--- ops/git_short_commit_id.sh
+++ ops/git_short_commit_id.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
 git_short_commit_id=`echo $CIRCLE_SHA1 | cut -b 1-8`
-
-echo $git_short_commit_id
-`export GIT_COMMIT="$git_short_commit_id"`
-
-echo $GIT_COMMIT
+echo export GIT_COMMIT=$git_short_commit_id > ops/sourcefile
