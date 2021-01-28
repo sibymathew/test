c70d67be8f819c254f206173b76f79043408a10a
20fc540b5acab646940f390a9a17184daae941f1 <siby.mathew@ruckuswireless.com>
2021-01-27 17:41:22 -0800

    Flask API for Attribute Collection

--- src/backend/plc.py
+++ src/backend/plc.py
@@ -4,7 +4,7 @@ def dreams_plc_find_info(plc_ip_addr):
   with LogixDriver(plc_ip_addr) as plc:
     return plc.info
 
-def dreams_plc_find_attributes(plc_ip_addr):
+def dreams_plc_find_attr(plc_ip_addr):
 
     #construct the API return object
     results = [] 
