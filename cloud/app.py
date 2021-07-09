8a8e15ab8cd43b533577452bfe8237b026062833
20fc540b5acab646940f390a9a17184daae941f1 <siby.mathew@ruckuswireless.com>
2021-07-09 13:51:27 -0700

    Added Cloud API

---
+++ cloud/app.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask import request
+from io import BytesIO
+import json
+import gzip
+import base64
+
+app = Flask(__name__)
+
+@app.route("/v1/data/push", methods = ['POST', 'GET'])
+def index():
+    resp = request.json
+    print(resp)
+    decodedData = base64.b64decode(resp["data"])
+    print(decodedData)
+
+    received_value = BytesIO(decodedData)
+    to_receive = gzip.GzipFile(fileobj=received_value, mode='rb').read()
+    payload_rx = json.loads(to_receive.decode("utf-8"))
+    print(payload_rx)
+
+    print(len(payload_rx))
+
+    return {"msg": "Hello World"}
+
+if __name__ == '__main__':
+   app.run(host="0.0.0.0", port=443, debug = True)
