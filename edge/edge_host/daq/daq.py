e16e97abb6666d32911c18a4a2e7ccb7ff70587f
20fc540b5acab646940f390a9a17184daae941f1 <siby.mathew@ruckuswireless.com>
2021-09-29 14:32:18 -0700

    Daq Driver

--- edge/edge_host/daq/daq.py
+++ edge/edge_host/daq/daq.py
@@ -1,19 +1,44 @@
 from subprocess import Popen, PIPE
+import time
 
 def check_signal(motor_list):
-	print("here")
 	resp = Popen(["./StaticDI"], stdout=PIPE, stderr=PIPE)
 	o, e = resp.communicate()
 	print(o)
 	print(e)
 
 	s = o.decode("utf-8")
-	print(type(s))
-	print(s)
+	time = round(time.time() * 1000)
 
 	if not "error" in s:
-	    print(s[0])
-	    print(s[1])
+	    if s[0] == "0":
+	    	msg = {}
+	    	stop_mode = 5
+            content = get_motor_data("table", motor_list, 0)
+
+            print(content)
+            if not content:
+                stop_mode = 5
+            else:
+                for row in json.loads(content):
+                    i = json.loads(row)
+                    k = ast.literal_eval(i["vfd_status"])
+                    if vfd_status == 3:
+                        stop_mode = 6
+                        break
+
+            msg["status"] = stop_mode
+            msg["timestamp"] = time
+            with open("/var/run/daq_status", "w") as hdlr:
+            	hdlr.write(msg)
+
+	    if s[1] == "0":
+	    	msg = {}
+	    	msg["status"] = 8
+	    	msg["timestamp"] = time
+
+	    	with open("/var/run/daq_status_edge_power", "w") as hdlr:
+	    		hdlr.write(msg)
 	else:
 	    print("Error")
 
---
+++ edge/edge_host/daq/edge_loader.py
@@ -0,0 +1,161 @@
+# pip install dse-driver
+from dse.cluster import Cluster, ExecutionProfile, EXEC_PROFILE_DEFAULT
+from dse.query import tuple_factory
+from dse.cluster import BatchStatement, SimpleStatement
+from dse.auth import PlainTextAuthProvider
+import uuid
+from datetime import timezone
+import datetime
+import calendar
+import json
+import os
+
+
+# Apache Cassandra DB connection (Edge)
+# TODO: CONFIG
+# EDGE_DB_INSTANCE = '127.0.0.1'
+EDGE_DB_PORT = 9042
+EDGE_DB_USER = 'cassandra'
+EDGE_DB_PASSWD = 'cassandra'
+EDGE_KEY_SPACE = 'edge_core'
+
+# Connect to database
+class DatabaseConnection:
+    def __init__(self):
+        self.edge_auth_provider = None              
+        self.edge_cluster = None
+        self.edge_session = None 
+        self.openSession()
+
+    def openSession(self):
+    
+        EDGE_DB_INSTANCE = os.environ["CASSANDRA_IP"]
+        self.edge_auth_provider = PlainTextAuthProvider(
+            username=EDGE_DB_USER, password=EDGE_DB_PASSWD)        
+        self.edge_cluster = Cluster(contact_points=[EDGE_DB_INSTANCE],port=EDGE_DB_PORT, auth_provider=self.edge_auth_provider)
+        self.edge_session = self.edge_cluster.connect(EDGE_KEY_SPACE)
+
+
+    def shutCluster(self):
+        self.edge_cluster.shutdown()
+
+
+def ingest_stream(crane_query_json):
+    #TODO: Log
+    
+    try:
+        
+        # Create a DB connection instance
+        dbSession = DatabaseConnection()
+        
+        #Check the given JSON is a list
+        if (isinstance(crane_query_json,list)):
+            #Loop thru the given JsON
+            print("List")
+        else:  
+            edge_uuid = crane_query_json["edge_uuid"]
+            # edge_mac =  crane_query_json["edge_mac"]
+            total_motors = crane_query_json["total_motors"]
+            query_timestamp= crane_query_json["timestamp"]        
+            motor_data = str(crane_query_json["motor_data"])
+            load_timestamp = datetime.datetime.now(timezone.utc)
+            motor_uuid = crane_query_json["motor_uuid"]
+
+            # single Insert Statement
+            dbSession.edge_session.execute(
+                    """
+                    insert into edge_core.crane_details (edge_uuid, total_motors, query_timestamp,  motor_uuid, motor_data,load_timestamp) 
+                    values (%s,%s,%s,%s,%s,%s)
+                    """,
+                    (edge_uuid, total_motors, query_timestamp, motor_uuid, motor_data, load_timestamp)
+                )
+
+        dbSession.shutCluster()
+        return "Stream Ingested"
+                        
+    except Exception as e:
+        error_msg = {"Status": "Failed to ingest for Edge UUID="+edge_uuid,"Error":str(e)}
+        return error_msg
+
+
+def ingest_stream2(crane_query_json):
+    # TODO: Log
+
+    try:
+
+        # Create a DB connection instance
+        dbSession = DatabaseConnection()
+
+        # Check the given JSON is a list
+        if (isinstance(crane_query_json, list)):
+            # Loop thru the given JsON
+            print("List")
+        else:
+            edge_uuid = crane_query_json["edge_uuid"]
+            # edge_mac =  crane_query_json["edge_mac"]
+            total_motors = crane_query_json["total_motors"]
+            query_timestamp = crane_query_json["timestamp"]
+            motor_data = str(crane_query_json["motor_data"])
+            # load_timestamp = datetime.datetime.today()
+            load_timestamp = datetime.datetime.now(timezone.utc)
+            motor_uuid = crane_query_json["motor_uuid"]
+
+            # single Insert Statement
+            dbSession.edge_session.execute(
+                """
+                insert into edge_core.crane_details2 (edge_uuid, total_motors, query_timestamp,  motor_uuid, motor_data,load_timestamp) 
+                values (%s,%s,%s,%s,%s,%s)
+                """,
+                (edge_uuid, total_motors, query_timestamp, motor_uuid, motor_data, load_timestamp)
+            )
+
+        dbSession.shutCluster()
+        return "Stream Ingested"
+
+    except Exception as e:
+        error_msg = {"Status": "Failed to ingest for Edge UUID=" + edge_uuid, "Error": str(e)}
+        return error_msg
+
+
+def get_motor_data(table_name,motor_list, interval):
+    # TODO: Log
+    # TODO: CONFIG
+
+    try:
+
+        # Create a DB connection instance
+        dbSession = DatabaseConnection()
+
+        if table_name is None:
+            table_name = 'edge_core.crane_details2'
+
+        if interval is None:
+            interval = 2
+
+        edge_uuid = 'b03108db-65f2-4d7c-b884-bb908d111400'
+
+        now = datetime.datetime.now(timezone.utc)
+        query_timestamp = now - datetime.timedelta(minutes=interval)
+        epoch_query_timestamp = str(calendar.timegm(query_timestamp.timetuple())) + '000'
+
+        motor_rows = []
+
+        for motor_id in motor_list:
+            if interval == 0:
+                motor_query = "select json edge_uuid, motor_uuid, query_timestamp,  load_timestamp, motor_data, total_motors from edge_core.crane_details where  motor_uuid = '" + motor_id + "' order by query_timestamp desc LIMIT 1"
+            else:
+                motor_query = "select json edge_uuid, motor_uuid, query_timestamp,  load_timestamp, motor_data, total_motors from edge_core.crane_details2 where  motor_uuid = '" + motor_id + "' and query_timestamp >= " + epoch_query_timestamp
+
+            for motor_row in dbSession.edge_session.execute(motor_query):
+                #motor_rows.append(motor_row[0].replace("'", '"'))
+                motor_rows.append(motor_row[0])
+                # print( motor_rows)
+                # print(json.dumps( motor_rows))
+                #
+
+        dbSession.shutCluster()
+        return json.dumps(motor_rows)
+
+    except Exception as e:
+        error_msg = {"Status": "Failed to pull data for Edge UUID=" + edge_uuid, "Error": str(e)}
+        return error_msg
