6b35b956488c538cc90cdf21cb1e8397d2b956e3
20fc540b5acab646940f390a9a17184daae941f1 <siby.mathew@ruckuswireless.com>
2021-11-02 10:15:05 -0700

    Config Sync UT Fixes

--- edge/edge_host/sync/config_sync.py
+++ edge/edge_host/sync/config_sync.py
@@ -7,11 +7,20 @@ from serial import Serial
 import ast
 import argparse
 import json
+import logging
+from logging.handlers import RotatingFileHandler
 
 from edge_loader import ingest_config, update_config_data
 import requests
 
-#import serial
+LOG_PATH = "/var/log/sync.log"
+log_hdlr = logging.getLogger(__name__)
+log_hdlr.setLevel(logging.DEBUG)
+
+hdlr = RotatingFileHandler(LOG_PATH,maxBytes=5 * 1024 * 1024, backupCount=20)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+hdlr.setFormatter(formatter)
+log_hdlr.addHandler(hdlr)
 
 #__EDGE_MAC__ = os.popen("ip addr show $(awk 'NR==3{print $1}' /proc/net/wireless | tr -d :) | awk '/ether/{print $2}'").read().rstrip()
 __EDGE_MAC__ = "00:0a:bb:11:22:22"
@@ -26,28 +35,31 @@ def getargs():
 
 def store(card, data, current_version):
 
+    applied_version = data["version"]
     print(ingest_config(data))
+    log_hdlr.info("Ingested Config to DB")
     res = requests.post("http://10.0.1.20:5000/cranes/version/update", json={"token": "def05f77d7541bfa8a9c61f551d45639"})
 
     print(res)
+    log_hdlr.info("FE API Call {}".format(res.content))
     if res.status_code == 400 or res.status_code == 401 or res.status_code == 500:
         return False
 
     if res.status_code == 200:
-        update_config_data(__EDGE_MAC__, data["version"], True)
-        update_config_data(__EDGE_MAC__, version, False)
+        update_config_data(__EDGE_MAC__, applied_version, True)
+        update_config_data(__EDGE_MAC__, current_version, False)
         to_send = {}
         to_send["req"] = "web.post"
         to_send["route"] = "configpullsuccess"
 
         data = {}
         data["edge_mac"] = __EDGE_MAC__
-        data["applied_version"] = version
+        data["applied_version"] = applied_version
         data["current_version"] = current_version
 
         to_send["body"] = data
         resp = card.Transaction(to_send)
-        print(resp)
+        log_hdlr.info("Cloud Flag Sync Response".format(resp))
     return True
 
 def main():
@@ -88,9 +100,8 @@ def main():
         while True:
             data["version"] = version
             to_send["body"] = data
-            print(to_send)
+            log_hdlr.info("Config Change Check Req: {}".format(to_send))
             resp = card.Transaction(to_send)
-            print(resp)
 
             try:
                 main_config = ast.literal_eval(resp["body"]["msg"])
@@ -106,7 +117,7 @@ def main():
                             update_flag = True
 
             if update_flag:
-                print(edge_config["config_data"])
+                log_hdlr.info("Config Change Check Resp: \n{}".format(edge_config["config_data"]))
                 config = ast.literal_eval(edge_config["config_data"])
                 with open("config.supervisor") as hdlr:
                     supervisor_conf = hdlr.read()
@@ -192,7 +203,7 @@ def main():
 
                 result = result + temp_tmpl
 
-                cmd = "python3 daq.py -mu {}".format(all_motor_list)
+                cmd = "sudo python3 daq.py -mu {}".format(all_motor_list)
                 name = "DAQ_Status"
                 dirs = "/home/utopia/test/edge/edge_host/daq"
 
@@ -202,12 +213,24 @@ def main():
 
                 result = result + temp_tmpl
 
+                cmd = "sudo python3 config_sync.py -puuid world.youtopian.siby.mathew:drill_bit -port /dev/ttyACM0  -rate 9600"
+                name = "Config_Sync"
+                dirs = "/home/utopia/test/edge/edge_host/sync"
+
+                temp_tmpl = supervisor_tmpl.replace("name", name)
+                temp_tmpl = temp_tmpl.replace("dirs", dirs)
+                temp_tmpl = temp_tmpl.replace("cmd", cmd)
+
+                result = result + temp_tmpl
 
                 with open("supervisord.conf", "w") as hdlr:
                     hdlr.write(result)
 
                 if store(card, edge_config, version):
-                    res = os.popen("supervisorctl restart all")
+                    res = os.popen("cp supervisord.conf /etc/supervisor/supervisord.conf")
+                    res = os.popen("supervisorctl reread")
+                    res = os.popen("supervisorctl reload")
+                    log_hdlr.info("Restarted Services")
                     with open("/etc/version", "w") as hdlr:
                         hdlr.write(str(edge_config["version"]))
                     
